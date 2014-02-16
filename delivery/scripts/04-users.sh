851d750d662b68b371d22ed7265b855d328ab614
Matti Kariluoma <matti@kariluo.ma>
2014-02-15 19:03:54 -0600

    enable boot to desktop

--- delivery/scripts/04-users.sh
+++ delivery/scripts/04-users.sh
@@ -8,3 +8,13 @@ su pi -c "tar xf $DELIVERY_DIR/files/pihome.tar.gz -C /home/pi/" || exit 1
 if [ -e "$SSH_KEY" ]; then
 	cp "$SSH_KEY" /home/pi/.ssh/authorized_keys || exit 1
 fi
+
+# enable boot to desktop
+update-rc.d lightdm enable 2 || exit 1
+sed /etc/lightdm/lightdm.conf -i -e "s/^#autologin-user=.*/autologin-user=pi/" || exit 1
+# disable raspi-config at boot
+# no || exit 1, ok to fail
+rm -f /etc/profile.d/raspi-config.sh
+sed -i /etc/inittab \
+		-e "s/^#\(.*\)#\s*RPICFG_TO_ENABLE\s*/\1/" \
+		-e "/#\s*RPICFG_TO_DISABLE/d"
